bc115c497377f66f3e583a402ffb0f4e2853e0cb
Igor Katsuba <ikatsuba@swiftcom.uk>
2018-09-12 01:32:19 +0300

    refactoring

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Languages, SortTypes } from '@core/enums';
-import { debounceTime } from 'rxjs/operators';
-import { BehaviorSubject, merge, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-filter',
@@ -27,17 +25,9 @@ export class FilterComponent {
     pageSize: [10],
     page: [1]
   });
-  // public values: Observable<any>;
-
 
   constructor(private fb: FormBuilder) {
 
-    // this.values = merge(
-    //   // new BehaviorSubject(this.form.value),
-    //   this.form.valueChanges
-    // ).pipe(
-    //   debounceTime(1000)
-    // );
   }
 
   public onPage({length, pageIndex, pageSize, previousPageIndex}) {
