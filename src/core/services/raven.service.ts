c8a72f0a02c83be3c200f3419709a8b95d8513a2
Igor Katsuba <ikatsuba@swiftcom.uk>
2018-11-01 11:11:32 +0300

    small refactoring

--- src/core/services/raven.service.ts
+++ src/core/services/raven.service.ts
@@ -1,28 +1,40 @@
-import { ErrorHandler, Inject, InjectionToken } from '@angular/core';
+import { ErrorHandler, Inject, Injectable, InjectionToken } from '@angular/core';
+import { captureException, captureMessage, init, Severity } from '@sentry/browser';
+
+export { Severity } from '@sentry/browser';
 
 export const RAVEN_DSN = new InjectionToken('RAVEN_DSN');
 
+@Injectable({
+  providedIn: 'root'
+})
 export class RavenService extends ErrorHandler {
   private raven;
 
   constructor(@Inject(RAVEN_DSN) private ravenDSN: string) {
     super();
+
+    if (this.ravenDSN) {
+      init({dsn: this.ravenDSN});
+    }
   }
 
   public async handleError(e) {
     if (this.ravenDSN) {
-      if (!this.raven) {
-        await this.initRaven();
-      }
-      this.raven.captureException(e);
+      captureException(e);
     }
 
     super.handleError(e);
   }
 
   private async initRaven() {
-    const Raven = await import('raven-js').then((m: any) => m.default);
-    Raven.config(this.ravenDSN).install();
-    this.raven = Raven;
+    init({dsn: this.ravenDSN});
   }
+
+  public captureMessage(message: string, level?: Severity) {
+    if (this.ravenDSN) {
+      captureMessage(message, level);
+    }
+  }
+
 }
