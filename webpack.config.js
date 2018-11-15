77cff7989432ea7b9414316d760a932f47acc3c5
Igor Katsuba <ikatsuba@swiftcom.uk>
2018-11-15 15:09:33 +0300

    ssr

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const webpack = require('webpack');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const {join} = require('path');
+
+
 module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'env.FB': process.env.FB,
       'env.NEWS': process.env.NEWS,
       'env.DSN': JSON.stringify(process.env.DSN)
+    }),
+    new PrerenderSPAPlugin({
+      staticDir: join(__dirname, 'dist/frontend-notes'),
+      routes: ['/'],
     })
   ]
 };
